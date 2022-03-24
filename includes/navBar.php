08c2a7aff5c3882446b9da472f6307ec39cfd3c0
coderFeedForwardAlg <gaspersscottma@gmail.com>
2022-03-23 20:42:13 -0400

    commit LAMP website

---
+++ includes/navBar.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">max's website</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="reports.php">reports</a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="pics.php"> about the creater  </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="ER.php"> ER diagram  </a>
+                    </li>
+                    
+                    
+                    <li class="nav-item">
+                        <a class="nav-link" href="signIn.php"> log out   </a>
+                    </li>
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
