d497505bf822623a6b91b0550f8963e29b00e113
coderFeedForwardAlg <76785692+coderFeedForwardAlg@users.noreply.github.com>
2022-06-11 21:28:14 -0400

    Update timer.php

--- timer.php
+++ timer.php
@@ -22,6 +22,11 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                        
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="suport.php">products</a>
+                    </li>
+                        
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="reports.php">reports</a>
                     </li>
