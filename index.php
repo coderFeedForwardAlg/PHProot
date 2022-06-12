4f2644dea778937d810a22f583f243b633cdf204
coderFeedForwardAlg <76785692+coderFeedForwardAlg@users.noreply.github.com>
2022-06-11 21:26:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -40,6 +40,10 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="suport.php">products</a>
+                    </li>
 
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="timer.php">timer</a>
